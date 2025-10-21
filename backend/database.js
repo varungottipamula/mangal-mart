af448d952c79d29b876f464a682ba913da23a28a
varungottipamula <vg3666603@gmail.com>
2025-10-22 02:51:33 +0530

    Update database.js and package.json

--- backend/database.js
+++ backend/database.js
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/mangal_mart", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB Connected (local Compass)");
-  } catch (err) {
-    console.error("❌ MongoDB connection error:", err);
+    console.log('MongoDB connected!');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
     process.exit(1);
   }
 };
 
-export default connectDB;  // ✅ Must use 'export default'
+export default connectDB;
